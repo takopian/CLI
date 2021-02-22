b667244a3ce37e3553c2797bdbd4c5d7668433f5
TigranAkopian <takopian98@gmail.com>
2021-02-22 13:28:25 +0300

    99

--- test/test_CLI.py
+++ test/test_CLI.py
@@ -16,5 +16,33 @@ def test_parser():
     assert result[0].args == ['5']
 
 
+def test_context():
+    input_line = 't=5'
+    context = src.expander.Expander()
+    parser = src.parser.Parser(context, input_line)
+    parser.parse()
+    assert context.context['t'].args == ['5']
+
+
+def test_expand():
+    commands = ['a=5', 'b=$(echo 123)', 'echo "$a" "$b"']
+    context = src.expander.Expander()
+    for line in commands[:2]:
+        parser = src.parser.Parser(context, line)
+        parser.parse()
+
+    print(context.context)
+    parser = src.parser.Parser(context, commands[2])
+    to_run = parser.parse()
+    print(to_run)
+    print(to_run[0].args)
+    context.expand(to_run[0])
+    print(to_run[0].args)
+    result = src.executor.Executor(to_run).execute()
+    assert result == '5 123'
+
+
 if __name__ == "__main__":
     test_parser()
+    test_context()
+    test_expand()
